784a9c84a50d89b83fcf7d17f8b07e6bacd55266
ariel <arielgenua24@gmail.com>
2022-09-05 13:57:19 -0300

    Finalice la creacion de la entidad products en la base de datos

---
+++ services/DB/migrations/20220905165154-create-products.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const { ProductSchema, PRODUCT_TABLE } = require('./../models/products.model')
+
+
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.createTable(PRODUCT_TABLE, ProductSchema)
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable(PRODUCT_TABLE, ProductSchema) 
+  }
+};
